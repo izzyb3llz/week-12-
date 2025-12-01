264d4654f2582685e467b13a9dab4789bafac519
izzyb3llz <isacontreras519@gmail.com>
2025-12-01 20:40:13 +0000

    Nested Lists, and List Comprehension

--- sets_tuples.py
+++ sets_tuples.py
@@ -17,3 +17,5 @@ tuple1 = (1, 2, 3, 4, 5)
 print(tuple1) # (1, 2, 3, 4, 5)
 # tuples are immutable, meaning they cannot be changed after creation
 # this makes tuples useful for storing data that should not be modified
+social_security_number = (123444, 444445, 5676789)
+print(social_security_number) # (123444, 444445, 5676789)
--- week 13  csp/4 — Nested Lists and List Comprehensions.py
+++ week 13  csp/4 — Nested Lists and List Comprehensions.py
@@ -1,4 +1,27 @@
-# Objective:
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+nested_list = [list1, list2]
+print(nested_list) # Output: [1,2, 3], {4, 5, 6]
+print(nested_list[1] [2])
+
+fruits = ["apple", "orange"< "banana", "coconut"]
+vegetables = ["celery", "carrot", "potato"]
+meats = ["chicken", "fish", "turkey"]
+groceries = [fruits, vegetables, meats]
+print(groceries[1][2])
+print(groceries[0][2]) # Output: banana
+for collection in groceries:
+    for food in collection:
+     print(food, end="")
+    print()
+num_pad = ((1, 2, 3),
+           (4, 5, 6),
+           (7, 8, 9),
+           ("", 0, "")) 
+
+
+# Objective;
 # Students will manipulate nested lists and understand basic list comprehensions.
 
 # Key Notes:
@@ -23,7 +46,7 @@ matrix = [
     [4, 5, 6],
     [7, 8, 9]
 ]
-
+print(matrix[0][2])
 print(matrix[1][2])    # 6
 
 # List comprehension
@@ -35,11 +58,19 @@ print(first_col)       # [1, 4, 7]
 # Practice Problems:
 
 # Build a matrix variable containing 3 lists of 3 numbers each.
+matrix = [
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90]
+]
 
 # Print the first list.
-
+print(matrix)
 # Print the second item from the third list.
-
+print(matrix[2][1])
 # Use a list comprehension to extract the last item from each sub-list.
-
+comprehension_list = [row[-1]for row in matrix]
+print(comprehension_list)
 # Challenge: Create a new list containing squares of numbers from 1–10 using a comprehension.
+squared_numbers = [x**2 for x in range(1, 11)]
+print(squared_numbers)
